9e08553303c219b8c7c4ff76f4bbe09802172d98
TuChunxu <steventcx@outlook.com>
2024-12-08 18:23:30 +0800

    prog1 and 2 done

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -1,41 +1,103 @@
+#include <math.h>
 #include <stdio.h>
 #include <thread>
 
 #include "CycleTimer.h"
 
 typedef struct {
-    float x0, x1;
-    float y0, y1;
-    unsigned int width;
-    unsigned int height;
-    int maxIterations;
-    int* output;
-    int threadId;
-    int numThreads;
+  float x0, x1;
+  float y0, y1;
+  unsigned int width;
+  unsigned int height;
+  int maxIterations;
+  int *output;
+  int threadId;
+  int numThreads;
 } WorkerArgs;
 
+extern void mandelbrotSerial(float x0, float y0, float x1, float y1, int width,
+                             int height, int startRow, int numRows,
+                             int maxIterations, int output[]);
 
-extern void mandelbrotSerial(
-    float x0, float y0, float x1, float y1,
-    int width, int height,
-    int startRow, int numRows,
-    int maxIterations,
-    int output[]);
+static inline int mandel(float c_re, float c_im, int count) {
+  float z_re = c_re, z_im = c_im;
+  int i;
+  for (i = 0; i < count; ++i) {
 
+    if (z_re * z_re + z_im * z_im > 4.f)
+      break;
+
+    float new_re = z_re * z_re - z_im * z_im;
+    float new_im = 2.f * z_re * z_im;
+    z_re = c_re + new_re;
+    z_im = c_im + new_im;
+  }
+
+  return i;
+}
+
+void workerThread_1(WorkerArgs *const args) {
+  // static float cumulative_time = 0;
+  // auto startTime = CycleTimer::currentSeconds();
+  unsigned int constexpr static NUM_ROWS = 32;
+  auto const height = args->height;
+  auto const id = args->threadId;
+  auto const n_block_rows = NUM_ROWS * args->numThreads;
+
+  for (auto startRow = id * NUM_ROWS; startRow < height;
+       startRow += n_block_rows) {
+    unsigned int numRows;
+    if (startRow + NUM_ROWS <= height) {
+      numRows = NUM_ROWS;
+    } else {
+      numRows = height - startRow;
+    }
+
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, args->width,
+                     args->height, startRow, numRows, args->maxIterations,
+                     args->output);
+  }
+  // auto endTime = CycleTimer::currentSeconds();
+  // cumulative_time += endTime - startTime;
+  // printf("[thread %d]:\t\t[%.3f] ms\n", id, cumulative_time * 1000);
+}
+
+void workerThread_2(WorkerArgs *const args) {
+  auto width = args->width;
+  auto height = args->height;
+  auto x0 = args->x0;
+  auto y0 = args->y0;
+
+  float dx = (args->x1 - x0) / width;
+  float dy = (args->y1 - y0) / height;
+  for (unsigned int line = args->threadId; line < args->height;
+       line += args->numThreads) {
+
+    for (unsigned int i = 0; i < width; ++i) {
+      float x = x0 + i * dx;
+      float y = y0 + line * dy;
+
+      int index = (line * width + i);
+      args->output[index] = mandel(x, y, args->maxIterations);
+    }
+  }
+}
 
 //
 // workerThreadStart --
 //
 // Thread entrypoint.
-void workerThreadStart(WorkerArgs * const args) {
+void workerThreadStart(WorkerArgs *const args) {
 
-    // TODO FOR CS149 STUDENTS: Implement the body of the worker
-    // thread here. Each thread should make a call to mandelbrotSerial()
-    // to compute a part of the output image.  For example, in a
-    // program that uses two threads, thread 0 could compute the top
-    // half of the image and thread 1 could compute the bottom half.
+  // TODO FOR CS149 STUDENTS: Implement the body of the worker
+  // thread here. Each thread should make a call to mandelbrotSerial()
+  // to compute a part of the output image.  For example, in a
+  // program that uses two threads, thread 0 could compute the top
+  // half of the image and thread 1 could compute the bottom half.
 
-    printf("Hello world from thread %d\n", args->threadId);
+  // workerThread_1(args);
+  workerThread_2(args);
+  printf("Hello world from thread %d\n", args->threadId);
 }
 
 //
@@ -43,54 +105,48 @@ void workerThreadStart(WorkerArgs * const args) {
 //
 // Multi-threaded implementation of mandelbrot set image generation.
 // Threads of execution are created by spawning std::threads.
-void mandelbrotThread(
-    int numThreads,
-    float x0, float y0, float x1, float y1,
-    int width, int height,
-    int maxIterations, int output[])
-{
-    static constexpr int MAX_THREADS = 32;
+void mandelbrotThread(int numThreads, float x0, float y0, float x1, float y1,
+                      int width, int height, int maxIterations, int output[]) {
+  static constexpr int MAX_THREADS = 32;
 
-    if (numThreads > MAX_THREADS)
-    {
-        fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
-        exit(1);
-    }
+  if (numThreads > MAX_THREADS) {
+    fprintf(stderr, "Error: Max allowed threads is %d\n", MAX_THREADS);
+    exit(1);
+  }
 
-    // Creates thread objects that do not yet represent a thread.
-    std::thread workers[MAX_THREADS];
-    WorkerArgs args[MAX_THREADS];
+  // Creates thread objects that do not yet represent a thread.
+  std::thread workers[MAX_THREADS];
+  WorkerArgs args[MAX_THREADS];
 
-    for (int i=0; i<numThreads; i++) {
-      
-        // TODO FOR CS149 STUDENTS: You may or may not wish to modify
-        // the per-thread arguments here.  The code below copies the
-        // same arguments for each thread
-        args[i].x0 = x0;
-        args[i].y0 = y0;
-        args[i].x1 = x1;
-        args[i].y1 = y1;
-        args[i].width = width;
-        args[i].height = height;
-        args[i].maxIterations = maxIterations;
-        args[i].numThreads = numThreads;
-        args[i].output = output;
-      
-        args[i].threadId = i;
-    }
+  for (int i = 0; i < numThreads; i++) {
 
-    // Spawn the worker threads.  Note that only numThreads-1 std::threads
-    // are created and the main application thread is used as a worker
-    // as well.
-    for (int i=1; i<numThreads; i++) {
-        workers[i] = std::thread(workerThreadStart, &args[i]);
-    }
-    
-    workerThreadStart(&args[0]);
+    // TODO FOR CS149 STUDENTS: You may or may not wish to modify
+    // the per-thread arguments here.  The code below copies the
+    // same arguments for each thread
+    args[i].x0 = x0;
+    args[i].y0 = y0;
+    args[i].x1 = x1;
+    args[i].y1 = y1;
+    args[i].width = width;
+    args[i].height = height;
+    args[i].maxIterations = maxIterations;
+    args[i].numThreads = numThreads;
+    args[i].output = output;
 
-    // join worker threads
-    for (int i=1; i<numThreads; i++) {
-        workers[i].join();
-    }
+    args[i].threadId = i;
+  }
+
+  // Spawn the worker threads.  Note that only numThreads-1 std::threads
+  // are created and the main application thread is used as a worker
+  // as well.
+  for (int i = 1; i < numThreads; i++) {
+    workers[i] = std::thread(workerThreadStart, &args[i]);
+  }
+
+  workerThreadStart(&args[0]);
+
+  // join worker threads
+  for (int i = 1; i < numThreads; i++) {
+    workers[i].join();
+  }
 }
-
--- prog2_vecintrin/main.cpp
+++ prog2_vecintrin/main.cpp
@@ -1,77 +1,78 @@
-#include <stdio.h>
+#include "CS149intrin.h"
+#include "logger.h"
 #include <algorithm>
 #include <getopt.h>
 #include <math.h>
-#include "CS149intrin.h"
-#include "logger.h"
+#include <stdio.h>
 using namespace std;
 
 #define EXP_MAX 10
 
 Logger CS149Logger;
 
-void usage(const char* progname);
-void initValue(float* values, int* exponents, float* output, float* gold, unsigned int N);
-void absSerial(float* values, float* output, int N);
-void absVector(float* values, float* output, int N);
-void clampedExpSerial(float* values, int* exponents, float* output, int N);
-void clampedExpVector(float* values, int* exponents, float* output, int N);
-float arraySumSerial(float* values, int N);
-float arraySumVector(float* values, int N);
-bool verifyResult(float* values, int* exponents, float* output, float* gold, int N);
+void usage(const char *progname);
+void initValue(float *values, int *exponents, float *output, float *gold,
+               unsigned int N);
+void absSerial(float *values, float *output, int N);
+void absVector(float *values, float *output, int N);
+void clampedExpSerial(float *values, int *exponents, float *output, int N);
+void clampedExpVector(float *values, int *exponents, float *output, int N);
+float arraySumSerial(float *values, int N);
+float arraySumVector(float *values, int N);
+bool verifyResult(float *values, int *exponents, float *output, float *gold,
+                  int N);
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
   int N = 16;
   bool printLog = false;
 
   // parse commandline options ////////////////////////////////////////////
   int opt;
-  static struct option long_options[] = {
-    {"size", 1, 0, 's'},
-    {"log", 0, 0, 'l'},
-    {"help", 0, 0, '?'},
-    {0 ,0, 0, 0}
-  };
+  static struct option long_options[] = {{"size", 1, 0, 's'},
+                                         {"log", 0, 0, 'l'},
+                                         {"help", 0, 0, '?'},
+                                         {0, 0, 0, 0}};
 
   while ((opt = getopt_long(argc, argv, "s:l?", long_options, NULL)) != EOF) {
 
     switch (opt) {
-      case 's':
-        N = atoi(optarg);
-        if (N <= 0) {
-          printf("Error: Workload size is set to %d (<0).\n", N);
-          return -1;
-        }
-        break;
-      case 'l':
-        printLog = true;
-        break;
-      case '?':
-      default:
-        usage(argv[0]);
-        return 1;
+    case 's':
+      N = atoi(optarg);
+      if (N <= 0) {
+        printf("Error: Workload size is set to %d (<0).\n", N);
+        return -1;
+      }
+      break;
+    case 'l':
+      printLog = true;
+      break;
+    case '?':
+    default:
+      usage(argv[0]);
+      return 1;
     }
   }
 
-
-  float* values = new float[N+VECTOR_WIDTH];
-  int* exponents = new int[N+VECTOR_WIDTH];
-  float* output = new float[N+VECTOR_WIDTH];
-  float* gold = new float[N+VECTOR_WIDTH];
+  float *values = new float[N + VECTOR_WIDTH];
+  int *exponents = new int[N + VECTOR_WIDTH];
+  float *output = new float[N + VECTOR_WIDTH];
+  float *gold = new float[N + VECTOR_WIDTH];
   initValue(values, exponents, output, gold, N);
 
   clampedExpSerial(values, exponents, gold, N);
   clampedExpVector(values, exponents, output, N);
 
-  //absSerial(values, gold, N);
-  //absVector(values, output, N);
+  // absSerial(values, gold, N);
+  // absVector(values, output, N);
 
   printf("\e[1;31mCLAMPED EXPONENT\e[0m (required) \n");
   bool clampedCorrect = verifyResult(values, exponents, output, gold, N);
-  if (printLog) CS149Logger.printLog();
+  if (printLog)
+    CS149Logger.printLog();
   CS149Logger.printStats();
 
-  printf("************************ Result Verification *************************\n");
+  printf("************************ Result Verification "
+         "*************************\n");
   if (!clampedCorrect) {
     printf("@@@ Failed!!!\n");
   } else {
@@ -91,18 +92,20 @@ int main(int argc, char * argv[]) {
       printf("Passed!!!\n");
     }
   } else {
-    printf("Must have N %% VECTOR_WIDTH == 0 for this problem (VECTOR_WIDTH is %d)\n", VECTOR_WIDTH);
+    printf("Must have N %% VECTOR_WIDTH == 0 for this problem (VECTOR_WIDTH is "
+           "%d)\n",
+           VECTOR_WIDTH);
   }
 
-  delete [] values;
-  delete [] exponents;
-  delete [] output;
-  delete [] gold;
+  delete[] values;
+  delete[] exponents;
+  delete[] output;
+  delete[] gold;
 
   return 0;
 }
 
-void usage(const char* progname) {
+void usage(const char *progname) {
   printf("Usage: %s [options]\n", progname);
   printf("Program Options:\n");
   printf("  -s  --size <N>     Use workload size N (Default = 16)\n");
@@ -110,24 +113,24 @@ void usage(const char* progname) {
   printf("  -?  --help         This message\n");
 }
 
-void initValue(float* values, int* exponents, float* output, float* gold, unsigned int N) {
+void initValue(float *values, int *exponents, float *output, float *gold,
+               unsigned int N) {
 
-  for (unsigned int i=0; i<N+VECTOR_WIDTH; i++)
-  {
+  for (unsigned int i = 0; i < N + VECTOR_WIDTH; i++) {
     // random input values
     values[i] = -1.f + 4.f * static_cast<float>(rand()) / RAND_MAX;
     exponents[i] = rand() % EXP_MAX;
     output[i] = 0.f;
     gold[i] = 0.f;
   }
-
 }
 
-bool verifyResult(float* values, int* exponents, float* output, float* gold, int N) {
+bool verifyResult(float *values, int *exponents, float *output, float *gold,
+                  int N) {
   int incorrect = -1;
   float epsilon = 0.00001;
-  for (int i=0; i<N+VECTOR_WIDTH; i++) {
-    if ( abs(output[i] - gold[i]) > epsilon ) {
+  for (int i = 0; i < N + VECTOR_WIDTH; i++) {
+    if (abs(output[i] - gold[i]) > epsilon) {
       incorrect = i;
       break;
     }
@@ -138,24 +141,28 @@ bool verifyResult(float* values, int* exponents, float* output, float* gold, int
       printf("You have written to out of bound value!\n");
     printf("Wrong calculation at value[%d]!\n", incorrect);
     printf("value  = ");
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
       printf("% f ", values[i]);
-    } printf("\n");
+    }
+    printf("\n");
 
     printf("exp    = ");
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
       printf("% 9d ", exponents[i]);
-    } printf("\n");
+    }
+    printf("\n");
 
     printf("output = ");
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
       printf("% f ", output[i]);
-    } printf("\n");
+    }
+    printf("\n");
 
     printf("gold   = ");
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
       printf("% f ", gold[i]);
-    } printf("\n");
+    }
+    printf("\n");
     return false;
   }
   printf("Results matched with answer!\n");
@@ -164,8 +171,8 @@ bool verifyResult(float* values, int* exponents, float* output, float* gold, int
 
 // computes the absolute value of all elements in the input array
 // values, stores result in output
-void absSerial(float* values, float* output, int N) {
-  for (int i=0; i<N; i++) {
+void absSerial(float *values, float *output, int N) {
+  for (int i = 0; i < N; i++) {
     float x = values[i];
     if (x < 0) {
       output[i] = -x;
@@ -175,18 +182,18 @@ void absSerial(float* values, float* output, int N) {
   }
 }
 
-
-// implementation of absSerial() above, but it is vectorized using CS149 intrinsics
-void absVector(float* values, float* output, int N) {
+// implementation of absSerial() above, but it is vectorized using CS149
+// intrinsics
+void absVector(float *values, float *output, int N) {
   __cs149_vec_float x;
   __cs149_vec_float result;
   __cs149_vec_float zero = _cs149_vset_float(0.f);
   __cs149_mask maskAll, maskIsNegative, maskIsNotNegative;
 
-//  Note: Take a careful look at this loop indexing.  This example
-//  code is not guaranteed to work when (N % VECTOR_WIDTH) != 0.
-//  Why is that the case?
-  for (int i=0; i<N; i+=VECTOR_WIDTH) {
+  //  Note: Take a careful look at this loop indexing.  This example
+  //  code is not guaranteed to work when (N % VECTOR_WIDTH) != 0.
+  //  Why is that the case?
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
 
     // All ones
     maskAll = _cs149_init_ones();
@@ -195,32 +202,32 @@ void absVector(float* values, float* output, int N) {
     maskIsNegative = _cs149_init_ones(0);
 
     // Load vector of values from contiguous memory addresses
-    _cs149_vload_float(x, values+i, maskAll);               // x = values[i];
+    _cs149_vload_float(x, values + i, maskAll); // x = values[i];
 
     // Set mask according to predicate
-    _cs149_vlt_float(maskIsNegative, x, zero, maskAll);     // if (x < 0) {
+    _cs149_vlt_float(maskIsNegative, x, zero, maskAll); // if (x < 0) {
 
     // Execute instruction using mask ("if" clause)
-    _cs149_vsub_float(result, zero, x, maskIsNegative);      //   output[i] = -x;
+    _cs149_vsub_float(result, zero, x, maskIsNegative); //   output[i] = -x;
 
     // Inverse maskIsNegative to generate "else" mask
-    maskIsNotNegative = _cs149_mask_not(maskIsNegative);     // } else {
+    maskIsNotNegative = _cs149_mask_not(maskIsNegative); // } else {
 
     // Execute instruction ("else" clause)
-    _cs149_vload_float(result, values+i, maskIsNotNegative); //   output[i] = x; }
+    _cs149_vload_float(result, values + i,
+                       maskIsNotNegative); //   output[i] = x; }
 
     // Write results back to memory
-    _cs149_vstore_float(output+i, result, maskAll);
+    _cs149_vstore_float(output + i, result, maskAll);
   }
 }
-
 
 // accepts an array of values and an array of exponents
 //
 // For each element, compute values[i]^exponents[i] and clamp value to
 // 9.999.  Store result in output.
-void clampedExpSerial(float* values, int* exponents, float* output, int N) {
-  for (int i=0; i<N; i++) {
+void clampedExpSerial(float *values, int *exponents, float *output, int N) {
+  for (int i = 0; i < N; i++) {
     float x = values[i];
     int y = exponents[i];
     if (y == 0) {
@@ -240,7 +247,7 @@ void clampedExpSerial(float* values, int* exponents, float* output, int N) {
   }
 }
 
-void clampedExpVector(float* values, int* exponents, float* output, int N) {
+void clampedExpVector(float *values, int *exponents, float *output, int N) {
 
   //
   // CS149 STUDENTS TODO: Implement your vectorized version of
@@ -249,13 +256,56 @@ void clampedExpVector(float* values, int* exponents, float* output, int N) {
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
-  
+
+  if (N < VECTOR_WIDTH) {
+    return clampedExpSerial(values, exponents, output, N);
+  }
+
+  __cs149_vec_float vec_values, vec_output, vec_res,
+      vec_threshold = _cs149_vset_float(9.999999f);
+  __cs149_vec_int vec_exponents, vec_zero = _cs149_vset_int(0),
+                                 vec_one = _cs149_vset_int(1);
+  __cs149_mask maskAll = _cs149_init_ones();
+
+  int start = N % VECTOR_WIDTH;
+  for (int i = start; i < N; i += VECTOR_WIDTH) {
+    __cs149_mask maskZero = _cs149_init_ones(0), maskPositive,
+                 maskExceed = _cs149_init_ones(0);
+
+    _cs149_vload_float(vec_values, values + i, maskAll);
+    _cs149_vload_int(vec_exponents, exponents + i, maskAll);
+
+    _cs149_veq_int(maskZero, vec_zero, vec_exponents, maskAll);
+    _cs149_vset_float(vec_res, 1.f, maskZero);
+
+    if (_cs149_cntbits(maskZero) != VECTOR_WIDTH) {
+
+      maskPositive = _cs149_mask_not(maskZero);
+      _cs149_vload_float(vec_res, values + i, maskPositive);
+
+      _cs149_vsub_int(vec_exponents, vec_exponents, vec_one, maskPositive);
+      _cs149_vlt_int(maskPositive, vec_zero, vec_exponents, maskPositive);
+
+      while (_cs149_cntbits(maskPositive)) {
+        _cs149_vmult_float(vec_res, vec_res, vec_values, maskPositive);
+
+        _cs149_vsub_int(vec_exponents, vec_exponents, vec_one, maskPositive);
+        _cs149_vlt_int(maskPositive, vec_zero, vec_exponents, maskPositive);
+      }
+      _cs149_vlt_float(maskExceed, vec_threshold, vec_res, maskAll);
+      _cs149_vset_float(vec_res, 9.999999f, maskExceed);
+    }
+    _cs149_vstore_float(output + i, vec_res, maskAll);
+  }
+  if (start) {
+    return clampedExpVector(values, exponents, output, VECTOR_WIDTH);
+  }
 }
 
 // returns the sum of all elements in values
-float arraySumSerial(float* values, int N) {
+float arraySumSerial(float *values, int N) {
   float sum = 0;
-  for (int i=0; i<N; i++) {
+  for (int i = 0; i < N; i++) {
     sum += values[i];
   }
 
@@ -265,16 +315,30 @@ float arraySumSerial(float* values, int N) {
 // returns the sum of all elements in values
 // You can assume N is a multiple of VECTOR_WIDTH
 // You can assume VECTOR_WIDTH is a power of 2
-float arraySumVector(float* values, int N) {
-  
-  //
-  // CS149 STUDENTS TODO: Implement your vectorized version of arraySumSerial here
-  //
-  
-  for (int i=0; i<N; i+=VECTOR_WIDTH) {
+float arraySumVector(float *values, int N) {
 
+  //
+  // CS149 STUDENTS TODO: Implement your vectorized version of arraySumSerial
+  // here
+  //
+
+  __cs149_vec_float res_vec = _cs149_vset_float(0.f), add_vec;
+  __cs149_mask maskAll = _cs149_init_ones();
+
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
+    _cs149_vload_float(add_vec, values + i, maskAll);
+    _cs149_vadd_float(res_vec, res_vec, add_vec, maskAll);
   }
 
-  return 0.0;
-}
+  int bits = 0;
+  while (VECTOR_WIDTH >> bits) {
+    ++bits;
+  }
+  --bits;
+  for (; bits; --bits) {
+    _cs149_hadd_float(res_vec, res_vec);
+    _cs149_interleave_float(res_vec, res_vec);
+  }
 
+  return res_vec.value[0];
+}
