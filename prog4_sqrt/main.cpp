a50755c474331452ef5d8835ac770cd7309b557c
tuchunxu <steventcx@outlook.com>
2024-12-10 18:21:33 +0800

    prog4 done.

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -1,103 +1,119 @@
-#include <stdio.h>
 #include <algorithm>
-#include <pthread.h>
 #include <math.h>
+#include <pthread.h>
+#include <stdio.h>
 
 #include "CycleTimer.h"
 #include "sqrt_ispc.h"
 
 using namespace ispc;
 
-extern void sqrtSerial(int N, float startGuess, float* values, float* output);
+extern void sqrtSerial(int N, float startGuess, float *values, float *output);
+extern void sqrtSerialAVX2(int N, float initialGuess, float values[],
+                           float output[]);
 
-static void verifyResult(int N, float* result, float* gold) {
-    for (int i=0; i<N; i++) {
-        if (fabs(result[i] - gold[i]) > 1e-4) {
-            printf("Error: [%d] Got %f expected %f\n", i, result[i], gold[i]);
-        }
+static void verifyResult(int N, float *result, float *gold) {
+  for (int i = 0; i < N; i++) {
+    if (fabs(result[i] - gold[i]) > 1e-4) {
+      printf("Error: [%d] Got %f expected %f\n", i, result[i], gold[i]);
     }
+  }
 }
 
 int main() {
 
-    const unsigned int N = 20 * 1000 * 1000;
-    const float initialGuess = 1.0f;
+  const unsigned int N = 20 * 1000 * 1000;
+  const float initialGuess = 1.0f;
 
-    float* values = new float[N];
-    float* output = new float[N];
-    float* gold = new float[N];
+  float *values = new float[N];
+  float *output = new float[N];
+  float *gold = new float[N];
 
-    for (unsigned int i=0; i<N; i++)
-    {
-        // TODO: CS149 students.  Attempt to change the values in the
-        // array here to meet the instructions in the handout: we want
-        // to you generate best and worse-case speedups
-        
-        // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
-    }
+  for (unsigned int i = 0; i < N; i++) {
+    // TODO: CS149 students.  Attempt to change the values in the
+    // array here to meet the instructions in the handout: we want
+    // to you generate best and worse-case speedups
 
-    // generate a gold version to check results
-    for (unsigned int i=0; i<N; i++)
-        gold[i] = sqrt(values[i]);
+    // starter code populates array with random input values
+    values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+    // values[i] = 1.f; // 2
+    // values[i] = i % 8 == 0 ? .001f : 1.f; // 3
+  }
 
-    //
-    // And run the serial implementation 3 times, again reporting the
-    // minimum time.
-    //
-    double minSerial = 1e30;
-    for (int i = 0; i < 3; ++i) {
-        double startTime = CycleTimer::currentSeconds();
-        sqrtSerial(N, initialGuess, values, output);
-        double endTime = CycleTimer::currentSeconds();
-        minSerial = std::min(minSerial, endTime - startTime);
-    }
+  // generate a gold version to check results
+  for (unsigned int i = 0; i < N; i++)
+    gold[i] = sqrt(values[i]);
 
-    printf("[sqrt serial]:\t\t[%.3f] ms\n", minSerial * 1000);
+  //
+  // And run the serial implementation 3 times, again reporting the
+  // minimum time.
+  //
+  double minSerial = 1e30;
+  for (int i = 0; i < 3; ++i) {
+    double startTime = CycleTimer::currentSeconds();
+    sqrtSerial(N, initialGuess, values, output);
+    double endTime = CycleTimer::currentSeconds();
+    minSerial = std::min(minSerial, endTime - startTime);
+  }
 
-    verifyResult(N, output, gold);
+  printf("[sqrt serial]:\t\t[%.3f] ms\n", minSerial * 1000);
 
-    //
-    // Compute the image using the ispc implementation; report the minimum
-    // time of three runs.
-    //
-    double minISPC = 1e30;
-    for (int i = 0; i < 3; ++i) {
-        double startTime = CycleTimer::currentSeconds();
-        sqrt_ispc(N, initialGuess, values, output);
-        double endTime = CycleTimer::currentSeconds();
-        minISPC = std::min(minISPC, endTime - startTime);
-    }
+  verifyResult(N, output, gold);
 
-    printf("[sqrt ispc]:\t\t[%.3f] ms\n", minISPC * 1000);
+  double minSerialAVX2 = 1e30;
+  for (int i = 0; i < 3; ++i) {
+    double startTime = CycleTimer::currentSeconds();
+    sqrtSerialAVX2(N, initialGuess, values, output);
+    double endTime = CycleTimer::currentSeconds();
+    minSerialAVX2 = std::min(minSerial, endTime - startTime);
+  }
 
-    verifyResult(N, output, gold);
+  printf("[sqrt serial avx2]:\t[%.3f] ms\n", minSerialAVX2 * 1000);
 
-    // Clear out the buffer
-    for (unsigned int i = 0; i < N; ++i)
-        output[i] = 0;
+  verifyResult(N, output, gold);
 
-    //
-    // Tasking version of the ISPC code
-    //
-    double minTaskISPC = 1e30;
-    for (int i = 0; i < 3; ++i) {
-        double startTime = CycleTimer::currentSeconds();
-        sqrt_ispc_withtasks(N, initialGuess, values, output);
-        double endTime = CycleTimer::currentSeconds();
-        minTaskISPC = std::min(minTaskISPC, endTime - startTime);
-    }
+  //
+  // Compute the image using the ispc implementation; report the minimum
+  // time of three runs.
+  //
+  double minISPC = 1e30;
+  for (int i = 0; i < 3; ++i) {
+    double startTime = CycleTimer::currentSeconds();
+    sqrt_ispc(N, initialGuess, values, output);
+    double endTime = CycleTimer::currentSeconds();
+    minISPC = std::min(minISPC, endTime - startTime);
+  }
 
-    printf("[sqrt task ispc]:\t[%.3f] ms\n", minTaskISPC * 1000);
+  printf("[sqrt ispc]:\t\t[%.3f] ms\n", minISPC * 1000);
 
-    verifyResult(N, output, gold);
+  verifyResult(N, output, gold);
 
-    printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
-    printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
+  // Clear out the buffer
+  for (unsigned int i = 0; i < N; ++i)
+    output[i] = 0;
 
-    delete [] values;
-    delete [] output;
-    delete [] gold;
+  //
+  // Tasking version of the ISPC code
+  //
+  double minTaskISPC = 1e30;
+  for (int i = 0; i < 3; ++i) {
+    double startTime = CycleTimer::currentSeconds();
+    sqrt_ispc_withtasks(N, initialGuess, values, output);
+    double endTime = CycleTimer::currentSeconds();
+    minTaskISPC = std::min(minTaskISPC, endTime - startTime);
+  }
 
-    return 0;
+  printf("[sqrt task ispc]:\t[%.3f] ms\n", minTaskISPC * 1000);
+
+  verifyResult(N, output, gold);
+
+  printf("\t\t\t\t(%.2fx speedup from avx2)\n", minSerial / minSerialAVX2);
+  printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial / minISPC);
+  printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial / minTaskISPC);
+
+  delete[] values;
+  delete[] output;
+  delete[] gold;
+
+  return 0;
 }
--- prog4_sqrt/sqrtSerial.cpp
+++ prog4_sqrt/sqrtSerial.cpp
@@ -1,29 +1,127 @@
+
 #include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <x86intrin.h>
 
+static const __m256 __m256_one_vec = _mm256_set1_ps(1.f);
+static const __m256 __m256_half_vec = _mm256_set1_ps(.5f);
+static const __m256 __m256_3_vec = _mm256_set1_ps(3.f);
+static const __m256 __m256_kThershold_vec = _mm256_set1_ps(0.00001f);
+static const __m256 __m256_abs_mask =
+    _mm256_castsi256_ps(_mm256_set1_epi32(0x7fffffff));
 
-void sqrtSerial(int N,
-                float initialGuess,
-                float values[],
-                float output[])
-{
+void sqrtSerial(int N, float initialGuess, float values[], float output[]) {
 
-    static const float kThreshold = 0.00001f;
+  static const float kThreshold = 0.00001f;
 
-    for (int i=0; i<N; i++) {
+  for (int i = 0; i < N; i++) {
 
-        float x = values[i];
-        float guess = initialGuess;
+    float x = values[i];
+    float guess = initialGuess;
 
-        float error = fabs(guess * guess * x - 1.f);
+    float error = fabs(guess * guess * x - 1.f);
 
-        while (error > kThreshold) {
-            guess = (3.f * guess - x * guess * guess * guess) * 0.5f;
-            error = fabs(guess * guess * x - 1.f);
-        }
-
-        output[i] = x * guess;
+    while (error > kThreshold) {
+      guess = (3.f * guess - x * guess * guess * guess) * 0.5f;
+      error = fabs(guess * guess * x - 1.f);
     }
+
+    output[i] = x * guess;
+  }
 }
 
+void sqrtSerialAVX2(int N, float initialGuess, float values[], float output[]) {
+
+  int end = N & ~(7);
+  int i = 0;
+
+work:
+  for (; i < end; i += 8) {
+    __m256 x = _mm256_loadu_ps(values + i);
+    __m256 guess = _mm256_set1_ps(initialGuess);
+
+    __m256 error = _mm256_and_ps(
+        _mm256_sub_ps(_mm256_mul_ps(_mm256_mul_ps(guess, guess), x),
+                      __m256_one_vec),
+        __m256_abs_mask);
+
+    __m256 gt_mask = _mm256_cmp_ps(error, __m256_kThershold_vec, _CMP_GT_OQ);
+
+    while (_mm256_movemask_ps(gt_mask)) {
+
+      guess = _mm256_blendv_ps(
+          guess,
+          _mm256_mul_ps(
+              _mm256_mul_ps(
+                  _mm256_sub_ps(__m256_3_vec,
+                                _mm256_mul_ps(_mm256_mul_ps(x, guess), guess)),
+                  guess),
+              __m256_half_vec),
+          gt_mask);
+
+      error = _mm256_and_ps(
+          _mm256_sub_ps(_mm256_mul_ps(_mm256_mul_ps(guess, guess), x),
+                        __m256_one_vec),
+          __m256_abs_mask);
+
+      gt_mask = _mm256_cmp_ps(error, __m256_kThershold_vec, _CMP_GT_OQ);
+    }
+    _mm256_storeu_ps(output + i, _mm256_mul_ps(x, guess));
+  }
+  if (N > end) {
+    i = N - 8;
+    end = N;
+    goto work;
+  }
+}
+
+void sqrtSerialAVX2Aligned(int N, float initialGuess, float values[],
+                           float output[]) {
+
+  // float *start_p = reinterpret_cast<float *>(
+  //           (reinterpret_cast<uintptr_t>(values) + 31) & (~31)),
+  //       *end_p = reinterpret_cast<float *>(
+  //           reinterpret_cast<uintptr_t>(values + N) & (~31));
+
+  int end = N & ~(7);
+  int i = 0;
+
+work:
+  for (; i < end; i += 8) {
+    __m256 x = _mm256_loadu_ps(values + i);
+    __m256 guess = _mm256_set1_ps(initialGuess);
+
+    __m256 error = _mm256_and_ps(
+        _mm256_sub_ps(_mm256_mul_ps(_mm256_mul_ps(guess, guess), x),
+                      __m256_one_vec),
+        __m256_abs_mask);
+
+    __m256 gt_mask = _mm256_cmp_ps(error, __m256_kThershold_vec, _CMP_GT_OQ);
+
+    while (_mm256_movemask_ps(gt_mask)) {
+
+      guess = _mm256_blendv_ps(
+          guess,
+          _mm256_mul_ps(
+              _mm256_mul_ps(
+                  _mm256_sub_ps(__m256_3_vec,
+                                _mm256_mul_ps(_mm256_mul_ps(x, guess), guess)),
+                  guess),
+              __m256_half_vec),
+          gt_mask);
+
+      error = _mm256_and_ps(
+          _mm256_sub_ps(_mm256_mul_ps(_mm256_mul_ps(guess, guess), x),
+                        __m256_one_vec),
+          __m256_abs_mask);
+
+      gt_mask = _mm256_cmp_ps(error, __m256_kThershold_vec, _CMP_GT_OQ);
+    }
+    _mm256_storeu_ps(output + i, _mm256_mul_ps(x, guess));
+  }
+  if (N > end) {
+    i = N - 8;
+    end = N;
+    goto work;
+  }
+}
